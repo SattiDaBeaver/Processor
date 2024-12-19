dd1ff3e10caecb79a7933c6bdfaf7ac5a369a730
SattiDaBeaver <saptarshitalukdar2305@gmail.com>
2024-12-18 22:17:28 -0500

    Updated Command Line Test

--- Assembler/Test/main.cpp
+++ Assembler/Test/main.cpp
@@ -8,8 +8,10 @@ int main(int argc, char* argv[]){
     // argc: number of arguments including "./main"
     // argv[]: array of the arguments in string format. 
 
+    // run "./main <arg1> <arg2>" in the command line
+
     cout << "Number of arguments: " << argc << endl;
-    
+
     for (int i = 0; i < argc; i++){
         cout << "Argument " << argc << " : " << argv[i] << endl;
     }
