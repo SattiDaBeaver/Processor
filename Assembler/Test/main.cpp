dbfef0a58ed78dad3b311ebaf2c07a21b7c39c9d
SattiDaBeaver <saptarshitalukdar2305@gmail.com>
2024-12-18 22:14:39 -0500

    Added Command Line Example Code

--- Assembler/Test/main.cpp
+++ Assembler/Test/main.cpp
@@ -5,7 +5,14 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+    // argc: number of arguments including "./main"
+    // argv[]: array of the arguments in string format. 
+
+    cout << "Number of arguments: " << argc << endl;
     
+    for (int i = 0; i < argc; i++){
+        cout << "Argument " << argc << " : " << argv[i] << endl;
+    }
 
     return 0;
 }
